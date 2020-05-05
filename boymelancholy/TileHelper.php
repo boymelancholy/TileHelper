5f35949b39da7cca13026e747e9ccab0a1a0d87f
boymelancholy <aov.luana@gmail.com>
2020-05-05 13:06:55 +0800

    更新

---
+++ boymelancholy/TileHelper.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace TileHelpers\boymelancholy;
+
+use pocketmine\tile\Chest;
+
+/**
+ * チェスト
+ *
+ * @author boymelancholy
+ */
+class TileHelper extends Chest
+{
+	/**
+	 * チェストの中身を並び変える
+	 */
+	public function sortContents() :void
+	{
+
+	}
+}
